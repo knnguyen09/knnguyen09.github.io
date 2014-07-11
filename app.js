bad1f2e5dd1cb883058b7f0722f661b3f6713ca3
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-10 22:12:24 -0500

    Testing

---
+++ app.js
@@ -0,0 +1,21 @@
+(function(){
+	var app = angular.module('store', ['store-products']);
+
+	app.controller('StoreController', [ '$http', function($http){
+		var store = this;
+		store.products = [];
+		$http.get('/products.json').success(function(data){
+			store.products = data;
+		});
+	}]);
+
+	app.controller("ReviewController", function(){
+		this.review = {};
+		this.review.createdOn = Date.now();
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			this.review = {};
+			this.review.createdOn = Date.now();
+		};
+	});
+})();
