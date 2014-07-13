c2c61134a4013aab014821b87c90d0f048e161c0
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-13 04:35:24 -0500

    Updated js files

--- MyPackage/MyPassword/VerifyPassword.js
+++ MyPackage/MyPassword/VerifyPassword.js
@@ -8,11 +8,12 @@ function verifyPassword() {
     var password=prompt("Please Enter Password:");
     if (password == "demo") {
         alert("Welcome to My Home Page!");
-        window.location = "http://khangnguyen.info/MyPackage/HomePage/HomePage.html";
+        window.location.href = "http://khangnguyen.info/MyPackage/HomePage/HomePage.html";
     }
     else {
-        window.location = "http://khangnguyen.info/MyPackage/MyPassword/WrongPassword.html";
+        window.location.href = "http://khangnguyen.info/MyPackage/MyPassword/WrongPassword.html";
     }
+	return false;
 }
 
 addEventListener("load", initiate);
