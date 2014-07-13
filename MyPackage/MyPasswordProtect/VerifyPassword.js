049feb0a41fc51b6df5574df6733377a90d1e481
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-13 01:23:20 -0500

    Update js file

--- MyPackage/MyPasswordProtect/VerifyPassword.js
+++ MyPackage/MyPasswordProtect/VerifyPassword.js
@@ -8,10 +8,10 @@ function verifyPassword() {
     var password=prompt("Please Enter Password:");
     if (password == "demo") {
         alert("Welcome to My Home Page!");
-        window.location = "MyPackage/HomePage/HomePage.html";
+        window.location = "location.host/MyPackage/HomePage/HomePage.html";
     }
     else {
-        window.location = "MyPackage/MyPasswordProtect/WrongPassword.html";
+        window.location = "location.host/MyPackage/MyPasswordProtect/WrongPassword.html";
     }
 }
 
