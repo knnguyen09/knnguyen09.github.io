aca71fd46b6961de405956cc0523f46783386fa5
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-13 00:36:06 -0500

    Push Part1

---
+++ MyPackage/MyPasswordProtect/VerifyPassword.js
@@ -0,0 +1,18 @@
+function initiate()
+{
+    var EnterSiteButton = document.getElementById('EnterSiteImage');
+    EnterSiteButton.addEventListener('click', verifyPassword);
+}
+
+function verifyPassword() {
+    var password=prompt("Please Enter Password:");
+    if (password == "demo") {
+        alert("Welcome to My Home Page!");
+        window.location = "MyPackage/HomePage/HomePage.html";
+    }
+    else {
+        window.location = "MyPackage/MyPasswordProtect/WrongPassword.html";
+    }
+}
+
+addEventListener("load", initiate);
