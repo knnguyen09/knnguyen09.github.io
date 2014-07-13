e758e7dfce3581c8559598ef5608e0b77975e729
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-13 04:55:26 -0500

    T

--- MyPackage/MyPassword/VerifyPassword.js
+++ MyPackage/MyPassword/VerifyPassword.js
@@ -1,5 +1,5 @@
 function verifyPassword() {
-    var password=prompt("Please Enter Password1:");
+    var password=prompt("Please Enter Password:");
     if (password == "demo") {
         alert("Welcome to My Home Page!");
         window.location = "MyPackage/HomePage/HomePage.html";
