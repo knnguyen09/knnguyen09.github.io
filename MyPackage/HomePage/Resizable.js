ddd7f21a70c2bc1990a8f98ce138e83923001e70
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-24 23:20:11 -0500

    T

---
+++ MyPackage/HomePage/Resizable.js
@@ -0,0 +1,26 @@
+function returnToiPhone5SSize()
+{
+    var width=678;
+	var height=500;
+	self.moveTo((screen.availwidth-width)/2,(screen.availheight-height)/2);
+	self.resizeTo(width,height);
+	return false;
+}
+
+function returnToiPadAirSize()
+{
+    var width=678;
+	var height=500;
+	self.moveTo((screen.availwidth-width)/2,(screen.availheight-height)/2);
+	self.resizeTo(width,height);
+	return false;
+}
+
+function returnToDesktopSize()
+{
+    var width=678;
+	var height=500;
+	self.moveTo((screen.availwidth-width)/2,(screen.availheight-height)/2);
+	self.resizeTo(width,height);
+	return false;
+}
