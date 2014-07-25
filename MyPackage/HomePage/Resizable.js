d91b76be26af44b2e0676e7b4271e128d836c4c3
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-25 00:28:17 -0500

    T

--- MyPackage/HomePage/Resizable.js
+++ MyPackage/HomePage/Resizable.js
@@ -11,16 +11,14 @@ function returnToiPadAirSize()
 {
     var width=678;
 	var height=500;
-	self.moveTo((screen.availwidth-width)/2,(screen.availheight-height)/2);
+	self.moveTo(0, 0);
 	self.resizeTo(width,height);
 	return false;
 }
 
 function returnToDesktopSize()
 {
-    var width=678;
-	var height=500;
-	self.moveTo((screen.availwidth-width)/2,(screen.availheight-height)/2);
-	self.resizeTo(width,height);
+	self.moveTo(0, 0);
+	self.resizeTo(screen.availWidth, screen.availHeight);
 	return false;
 }
