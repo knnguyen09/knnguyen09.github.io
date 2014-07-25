626826a6c8af63984f3fa4387320fc647aaedafd
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-25 00:19:22 -0500

    T

--- MyPackage/MyPassword/VerifyPassword.js
+++ MyPackage/MyPassword/VerifyPassword.js
@@ -2,7 +2,7 @@ function verifyPassword() {
     var password=prompt("Password: demo");
     if (password == "demo") {
         alert("Welcome to My Home Page!");
-		var myWindow = window.open("", "", "width=200, height=100");
+		var myWindow = window.open("MyPackage/HomePage/HomePage.html", "", "width=screen.availwidth, height=screen.availheight");
     }
     else {
         window.location = "MyPackage/MyPassword/WrongPassword.html";
