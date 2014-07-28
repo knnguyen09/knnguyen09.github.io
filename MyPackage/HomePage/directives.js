f2a5ed4fa11f2a37eee54e4d701e9f6fe62b056b
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-27 20:27:40 -0500

    T

--- MyPackage/HomePage/directives.js
+++ MyPackage/HomePage/directives.js
@@ -4,16 +4,7 @@
 	app.directive('mobilePanels', function(){
 		return{
 			restrict: 'E',
-			templateUrl: 'mobile-panels.html',
-			controller: function(){
-				this.selectTab = function(setTab){
-					this.tab = setTab || 0;
-				};
-				this.isSelected = function(checkTab){
-					return this.tab === checkTab;
-				};
-			},
-			controllerAs: 'panel'
+			templateUrl: 'mobile-panels.html'
 		};
 	});
 })();
