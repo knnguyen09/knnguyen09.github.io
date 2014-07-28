6260ec5b4054feb3fd41791b20ec4ab8f6b52e2a
knnguyen09 <knnguyen09@users.noreply.github.com>
2014-07-27 20:27:27 -0500

    T

--- MyPackage/HomePage/Skills/directives.js
+++ MyPackage/HomePage/Skills/directives.js
@@ -16,4 +16,20 @@
 			controllerAs: 'panel'
 		};
 	});
+	
+	app.directive('mobilePanels', function(){
+		return{
+			restrict: 'E',
+			templateUrl: '../mobile-panels.html',
+			controller: function(){
+				this.selectTab = function(setTab){
+					this.tab = setTab || 0;
+				};
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				};
+			},
+			controllerAs: 'panel'
+		};
+	});
 })();
